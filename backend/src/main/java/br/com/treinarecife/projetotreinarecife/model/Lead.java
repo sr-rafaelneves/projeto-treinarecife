a32981fae3fdb7d21a2b29ffbe895391f1ef92d1
flavioxenta <xentapps@yahoo.com.br>
2022-11-17 20:31:12 -0300

    TR001 criaçao da entidade Lead

---
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Lead.java
@@ -0,0 +1,56 @@
+package br.com.treinarecife.projetotreinarecife.model;
+
+import java.util.Date;
+
+public class Lead {
+    
+    private Integer idLead;
+    private String nome;
+    private String telefone;
+    private Date dataCadastro;
+    private String status;
+    private Date novoContato;
+    private String observacao;
+    public Integer getIdLead() {
+        return idLead;
+    }
+    public void setIdLead(Integer idLead) {
+        this.idLead = idLead;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Date getNovoContato() {
+        return novoContato;
+    }
+    public void setNovoContato(Date novoContato) {
+        this.novoContato = novoContato;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}
