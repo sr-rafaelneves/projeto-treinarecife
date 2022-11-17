8ff3df942fb6675bee7a390e95b5d306ab8e034d
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-11-17 19:52:47 -0300

    Upload do Projeto Back End no repo Main

---
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/ProjetoTreinarecifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinarecife.projetotreinarecife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTreinarecifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTreinarecifeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/treinarecife/projetotreinarecife/ProjetoTreinarecifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinarecife.projetotreinarecife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTreinarecifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
