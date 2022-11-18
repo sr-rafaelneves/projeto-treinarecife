c3af381c77fb70aa2aa3949080caf47dde0a227f
flavioxenta <xentapps@yahoo.com.br>
2022-11-17 21:17:52 -0300

    TR001 correçao aplicada conforme o P.O

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Aluno.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Aluno.java
@@ -1,7 +1,16 @@
 package br.com.treinarecife.projetotreinarecife.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_aluno")
 public class Aluno {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idAluno;
     private String nome;
     private String telefone;
