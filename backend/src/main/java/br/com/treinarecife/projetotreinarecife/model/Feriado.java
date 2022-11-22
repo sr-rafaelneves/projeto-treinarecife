6d62f40ad6e30a675bff86a08e97bc70adcad918
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-11-22 20:38:57 -0300

    TR006 - Alteração de PK

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Feriado.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Feriado.java
@@ -3,12 +3,18 @@ package br.com.treinarecife.projetotreinarecife.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_feriado")
 public class Feriado {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO);
+    private int id;
     private Date dataFeriado;
     private String descricao;
 
@@ -24,7 +30,19 @@ public class Feriado {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public Feriado(int id, String descricao, Date dataFeriado ){
+        this.id = id;
+        this.descricao = descricao;
+        this.dataFeriado = dataFeriado;
+    }
+  
     
     
 }
