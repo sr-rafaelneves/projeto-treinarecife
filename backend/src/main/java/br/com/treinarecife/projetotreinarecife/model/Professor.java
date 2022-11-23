15e0a8a80d8fe0f8377be3d9c4ec57209de46c0d
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-11-22 21:03:31 -0300

    TR025 - Correção de Annotation da classe Professor

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Professor.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Professor.java
@@ -1,11 +1,22 @@
 package br.com.treinarecife.projetotreinarecife.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_professor")
 public class Professor {
-    
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
 private Integer idProfessor;
 private String nome;
 private String telefone;
 private String valorhoraaula;
+
 public Integer getIdProfessor() {
     return idProfessor;
 }
