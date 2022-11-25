cc323377df27dfd7033c7569b33dca88005798ee
flavioxenta <xentapps@yahoo.com.br>
2022-11-25 19:37:50 -0300

    TR025 - foi adicionado mapping de relacionamento de aluno para turma

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Aluno.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/model/Aluno.java
@@ -1,9 +1,14 @@
 package br.com.treinarecife.projetotreinarecife.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,6 +17,16 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idAluno;
+
+    @OneToMany
+    @JoinColumn(name= "idTurma")
+    private List<Turma> turmas;
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
     private String nome;
     private String telefone;
     private String endereco;
