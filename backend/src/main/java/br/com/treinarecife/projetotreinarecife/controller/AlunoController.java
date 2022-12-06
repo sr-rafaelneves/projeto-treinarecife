db4f9494a67474c866ad44458467af49af7a1f34
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-12-06 20:23:27 -0300

    TR999 - Atualização do AlunoController

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/controller/AlunoController.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/controller/AlunoController.java
@@ -2,7 +2,9 @@ package br.com.treinarecife.projetotreinarecife.controller;
 
 import java.util.List;
 
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
