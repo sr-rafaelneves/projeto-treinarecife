4efa117ae175caef97d6e6312691a5ea630e8e75
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-12-06 20:37:50 -0300

    TR999 - Adicionado CursoController

--- backend/src/main/java/br/com/treinarecife/projetotreinarecife/controller/CursoController.java
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/controller/CursoController.java
@@ -1,13 +1,19 @@
 package br.com.treinarecife.projetotreinarecife.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import br.com.treinarecife.projetotreinarecife.model.Curso;
 import br.com.treinarecife.projetotreinarecife.repositories.CursoRepository;
 
 @RestController
-@RequestMapping(value = "/Curso")
+@RequestMapping(value = "/curso")
 public class CursoController {
     
     @Autowired
