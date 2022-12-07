899960e2be7291a3147664b028bdec04781f197a
Rafael A N Silva <sr.rafaelaugusto@gmail.com>
2022-12-06 21:35:38 -0300

    TR998 - Criado DiaaulaRepository faltando

---
+++ backend/src/main/java/br/com/treinarecife/projetotreinarecife/repositories/DiaaulaRepository.java
@@ -0,0 +1,8 @@
+package br.com.treinarecife.projetotreinarecife.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.treinarecife.projetotreinarecife.model.DiaAula;
+
+public interface DiaaulaRepository extends JpaRepository <DiaAula, Integer>{
+
+}
